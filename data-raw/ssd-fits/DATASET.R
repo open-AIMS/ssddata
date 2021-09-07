2520286ebd8cc1f9fd9b90c0273b7588e8073d61
beckyfisher <rebeccafisher76@gmail.com>
2021-09-07 11:50:31 +0800

    Added new fits data for current cran version of ssdtools and default shiny distributions

--- data-raw/ssd-fits/DATASET.R
+++ data-raw/ssd-fits/DATASET.R
@@ -17,7 +17,7 @@ library(usethis)
 library(dplyr)
 library(tidyr)
 
-ssd_fits <- read_csv("data-raw/ssd-fits/ssd-fits.csv",
+ssd_fits <- read_csv("data-raw/ssd-fits/ssd-fits.csv", 
   col_types = cols(
     Dataset = col_character(),
     Filter = col_character(),
@@ -35,60 +35,63 @@ ssd_fits <- read_csv("data-raw/ssd-fits/ssd-fits.csv",
 )
 
 
-# # Append fit data from May 7th 2021
-# load("data-raw/ssd-fits/ssd-fit-data/ssdtools_v0.3.32021-05-12burrlioz_fits_batch.RData")
-# load("data-raw/ssd-fits/ssd-fit-data/ssdtools_v0.3.32021-05-12shiny_default.RData")
-# 
-# 
-# all_ssd_fits[[1]]$hc_out
-# summary(all_ssd_fits[[1]]$dist[[1]])
-# names(all_ssd_fits)
-# 
-# new_fits[[1]]$hc_out
-# summary(new_fits[[1]]$dist[[1]])
-# names(new_fits)
-# 
-# all_fits_add <- bind_rows(lapply(all_ssd_fits, FUN = function(x) {
-#   x$hc_out
-# }), .id = "Dataset") %>%
-#   dplyr::mutate(
-#     Notes = "llogis-gamma-lnorm model averaged fit with 10000 bootstrap iterations. Used geometric mean of multiple species (if relevant)",
-#     Filter = NA,
-#     Software = "ssdtools",
-#     Version = "0.3.3",
-#     Distribution = "averaged",
-#     PC = 100 - percent,
-#     Estimate = est,
-#     SE = se,
-#     Lower = lcl,
-#     Upper = ucl,
-#     Reference = "data-raw/ssd-fits/ssd-fit-data/ssdtools_v0.3.32021-05-12shiny_default.RData"
-#   ) %>%
-#   dplyr::select(all_of(colnames(ssd_fits)))
-# 
-# new_fits_add <- cbind(do.call("rbind", lapply(new_fits, FUN = function(x) {
-#   x$hc_out
-# })), burrlioz_fits) %>%
-#   dplyr::mutate(
-#     Notes = paste(
-#       sapply(new_fits, FUN = function(x) {
-#         paste(names(x$dist), collapse = "-")
-#       }),
-#       " model averaged fit with 10000 bootstrap iterations. Used geometric mean of multiple species (if relevant)"
-#     ),
-#     Software = "ssdtools",
-#     Version = "0.3.3",
-#     Distribution = "averaged",
-#     Estimate = est,
-#     SE = se,
-#     Lower = lcl,
-#     Upper = ucl,
-#     Reference = "data-raw/ssd-fits/ssd-fit-data/ssdtools_v0.3.32021-05-12burrlioz_fits_batch.RData"
-#   ) %>%
-#   dplyr::select(all_of(colnames(ssd_fits)))
-# 
-# ssd_fits <- rbind(ssd_fits, all_fits_add, new_fits_add) %>%
-#   arrange(Dataset, Filter, PC) %>%
-#   distinct()
+# Append fit data from May 7th 2021
+load("data-raw/ssd-fits/ssd-fit-data/ssddata_ssdtools_v0.3.42021-09-02burrlioz_fits_batch.RData")
+load("data-raw/ssd-fits/ssd-fit-data/ssddata_ssdtools_v0.3.42021-09-02shiny_default.RData")
+
+
+all_ssd_fits[[1]]$hc_out
+summary(all_ssd_fits[[1]]$dist[[1]])
+names(all_ssd_fits)
+
+new_fits[[1]]$hc_out
+summary(new_fits[[1]]$dist[[1]])
+names(new_fits)
+
+all_fits_add <- bind_rows(lapply(all_ssd_fits, FUN = function(x) {
+  x$hc_out
+}), .id = "Dataset") %>%
+  dplyr::mutate(
+    Notes = "llogis-gamma-lnorm model averaged fit with 10000 bootstrap iterations. Used geometric mean of multiple species (if relevant)",
+    Filter = NA,
+    Software = "ssdtools",
+    Version = "0.3.4",
+    Distribution = "averaged",
+    PC = 100 - percent,
+    Estimate = est,
+    SE = se,
+    Lower = lcl,
+    Upper = ucl,
+    Reference = "data-raw/ssd-fits/ssd-fit-data/ssddata_ssdtools_v0.3.42021-09-shiny_default.RData"
+  ) %>%
+  dplyr::select(all_of(colnames(ssd_fits)))
+
+new_fits_add <- cbind(do.call("rbind", lapply(new_fits, FUN = function(x) {
+  x$hc_out
+})), burrlioz_fits) %>%
+  dplyr::mutate(
+    Notes = paste(
+      sapply(new_fits, FUN = function(x) {
+        paste(names(x$dist), collapse = "-")
+      }),
+      " model averaged fit with 10000 bootstrap iterations. Used geometric mean of multiple species (if relevant)"
+    ),
+    Software = "ssdtools",
+    Version = "0.3.4",
+    Distribution = "averaged",
+    Estimate = est,
+    SE = se,
+    Lower = lcl,
+    Upper = ucl,
+    Reference = "data-raw/ssd-fits/ssd-fit-data/ssddata_ssdtools_v0.3.42021-09-02burrlioz_fits_batch.RData"
+  ) %>%
+  dplyr::select(all_of(colnames(ssd_fits)))
+
+
+updated_ssd_fits <- rbind(ssd_fits, all_fits_add, new_fits_add) %>%
+  arrange(Dataset, Filter, PC) %>%
+  distinct()
+
+ssd_fits <- updated_ssd_fits
 
 use_data(ssd_fits, overwrite = TRUE)
