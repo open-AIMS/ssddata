b1ec2968007b3f2784ac6bb97118eb614f62c1bf
Ayla Pearson <ayla@poissonconsulting.ca>
2025-10-31 10:58:40 -0700

    adding script to create database

---
+++ data-raw/wqbench/DATASET.R
@@ -0,0 +1,20 @@
+library(wqbench)
+library(tidyverse)
+
+# run when there is a new database to download
+data_set <- wqb_create_data_set(
+  file_path = file.path("data-raw", "wqbench"),
+  folder_path = file.path("data-raw", "wqbench")
+)
+
+# if the database has already been downloaded and the data set is created
+data_set <- readRDS(file.path("data-raw", "wqbench", "ecotox_ascii_09_11_2025.rds"))
+
+data_wqbench <- 
+  data_set %>% 
+  group_by(cas) %>% 
+  filter(5 < n()) %>% 
+  ungroup()
+
+# remove this comment and run once we have decided on the data set
+# usethis::use_data(data_wqbench, overwrite = TRUE)
