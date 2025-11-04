e5ed5b28cad3608195a12e4e0d48417b01bd7b66
Ayla Pearson <ayla@poissonconsulting.ca>
2025-11-04 13:43:24 -0800

    aggregate dataset

--- data-raw/wqbench/DATASET.R
+++ data-raw/wqbench/DATASET.R
@@ -10,11 +10,22 @@ data_set <- wqb_create_data_set(
 # if the database has already been downloaded and the data set is created
 data_set <- readRDS(file.path("data-raw", "wqbench", "ecotox_ascii_09_11_2025.rds"))
 
-data_wqbench <- 
+data_set <- 
   data_set %>% 
   group_by(cas) %>% 
   filter(5 < n()) %>% 
   ungroup()
+
+data_wqbench_groups <- 
+  data_set %>% 
+  # set all to SSD
+  mutate(method = "SSD") %>% 
+  group_split(cas)
+
+aggregated_data <- map(data_wqbench_groups, wqbench::wqb_aggregate)
+
+data_wqbench <- bind_rows(aggregated_data)
+
 
 # remove this comment and run once we have decided on the data set
 # usethis::use_data(data_wqbench, overwrite = TRUE)
