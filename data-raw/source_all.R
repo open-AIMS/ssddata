c7545b4bbeafa9ce22e2a33a4977059f1d929dbf
Rebecca Fisher <r.fisher@aims.gov.au>
2026-01-23 10:40:32 +0800

    Ensure wqbench DATASET.R not automatically sourced

--- data-raw/source_all.R
+++ data-raw/source_all.R
@@ -2,6 +2,9 @@
 files <- unlist(sapply(list.dirs("data-raw")[-1], FUN = function(x) {
   list.files(x, pattern = "[.][rR]$", full.names = TRUE)
 }))
+# remove wqbench because this is documented manually
+files <- setdiff(files, "data-raw/wqbench/DATASET.R")
+
 invisible(lapply(files, source))
 roxygen2md::roxygen2md()
 # styler::style_pkg(filetype = c("R", "Rmd"))
