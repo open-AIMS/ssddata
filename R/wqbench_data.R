c12c36b48fb1ee7753a47b21aa9b4eaf46abeaa3
Rebecca Fisher <r.fisher@aims.gov.au>
2026-05-19 13:21:13 +0800

    Minor fix to prevent pkgdown error

--- R/wqbench_data.R
+++ R/wqbench_data.R
@@ -1,7 +1,6 @@
 #' Species Sensitivity Data from US EPA ECOTOX Database
 #'
-#' This dataset contains \Sexpr{data(wqbench_data, package = "ssddata");
-#' length(unique(wqbench_data$cas))} different chemicals. The data are from
+#' This dataset contains species sensitivity records for multiple chemicals. The data are from
 #' the US EPA ECOTOX database but are cleaned and standardized by the
 #' [`wqbench`](https://github.com/bcgov/wqbench/) package.
 #'
@@ -62,10 +61,21 @@
 #'
 #' @name wqbench_data
 #' @docType data
-#' @format An object of class `tbl_df` (inherits from `tbl`, `data.frame`) with
-#'   \Sexpr{data(wqbench_data, package = "ssddata"); nrow(wqbench_data)} rows
-#'   and \Sexpr{data(wqbench_data, package = "ssddata"); ncol(wqbench_data)}
-#'   columns.
+#' @format A tibble with columns:
+#' \describe{
+#' \item{chemical_name}{The chemical common name (chr).}
+#' \item{cas}{The chemical cas number (chr).}
+#' \item{latin_name}{The species latin name (chr).}
+#' \item{common_name}{The species common name (chr).}
+#' \item{effect}{The effect that was being tested (chr).}
+#' \item{sp_aggre_conc_mg.L}{The chemical concentration in micrograms per litre (dbl).}
+#' \item{trophic_group}{Trophic group of species (fct).}
+#' \item{ecological_group}{Identification of salmonids and planktonic invertebrates; otherwise "other" (fct).}
+#' \item{species_present_in_bc}{Whether species is present in British Columbia, Canada (logi).}
+#' \item{class}{The class name (chr).}
+#' \item{tax_order}{The order name (chr).}
+#' \item{family}{The family name (chr).}
+#' }
 #' @keywords datasets
 #' @examples
 #'
