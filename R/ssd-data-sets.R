4ef95ed200244f49b6006c5650a1decf6dcd581a
Rebecca Fisher <r.fisher@aims.gov.au>
2026-05-20 15:40:10 +0800

    update details to reflect this function will NOT return envirotox data

--- R/ssd-data-sets.R
+++ R/ssd-data-sets.R
@@ -1,12 +1,14 @@
 #' Species Sensitivity Data Sets
-#' 
-#' @description 
-#' Extracts the data sets for all individually documented chemicals in ssddata. 
-#' 
+#'
+#' @description
+#' Extracts the data sets for all individually documented chemicals in ssddata.
+#'
 #' @details
 #' Note only datasets for chemicals that are individually documented are returned.
-#' This does not include chemicals from the wqbench US EPA Ecotox database. 
-#' Data from wqbench can be retrieved using data("wqbench_data")
+#' This does not include chemicals from the wqbench US EPA Ecotox database.
+#' Data from wqbench can be retrieved using data("wqbench_data").
+#' This does not include the envirotox datasets which span multiple chemicals.
+#' Data from envirotox can be retrieved using data("envirotox_data").
 #'
 #' @return A named list of the available data sets for individual chemicals.
 #' @export
@@ -14,11 +16,14 @@
 #' @examples
 #' ssd_data_sets()
 ssd_data_sets <- function() {
-  items <- utils::data(package="ssddata")$results[, "Item"]
+  items <- utils::data(package = "ssddata")$results[, "Item"]
   items <- items[!items %in% c("ssd_fits")]
   items <- items[!grepl("_data$", items)]
+  items <- items[!grepl("^envirotox_", items)]
   items <- sort(items)
-  datasets <- lapply(items, function(x) eval(parse(text = paste0("ssddata::", x))))
+  datasets <- lapply(items, function(x) {
+    eval(parse(text = paste0("ssddata::", x)))
+  })
   names(datasets) <- items
   datasets
 }
