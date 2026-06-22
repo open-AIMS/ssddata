805a6eb8d622305dcf9db3d8bbd3b1d7897d1050
Rebecca Fisher <r.fisher@aims.gov.au>
2026-06-22 13:55:36 +0800

    Add media type to wqbench data

--- data-raw/wqbench/DATASET.R
+++ data-raw/wqbench/DATASET.R
@@ -2,11 +2,37 @@ library(wqbench)
 library(tidyverse)
 library(readwritesqlite)
 
+.map_wqbench_medium <- function(media_type) {
+  media_type |>
+    stringr::str_remove("/$") |>
+    dplyr::case_match(
+      "FW" ~ "Freshwater",
+      "SW" ~ "Marine",
+      .default = NA_character_
+    )
+}
+
+.build_wqbench_medium_lookup <- function(data_set) {
+  data_set |>
+    transmute(
+      Chemical = chemical_name,
+      Species = latin_name,
+      effect = effect,
+      Medium = .map_wqbench_medium(media_type)
+    ) |>
+    filter(!is.na(Medium)) |>
+    group_by(Chemical, Species, effect) |>
+    summarise(
+      Medium = if_else(n_distinct(Medium) == 1L, first(Medium), "Unknown"),
+      .groups = "drop"
+    )
+}
+
 # run when there is a new database to download or you haven't downloaded one yet
-data_set <- wqb_create_data_set(
-  file_path = file.path("data-raw", "wqbench"),
-  folder_path = file.path("data-raw", "wqbench")
-)
+# data_set <- wqb_create_data_set(
+#   file_path = file.path("data-raw", "wqbench"),
+#   folder_path = file.path("data-raw", "wqbench")
+# )
 
 # if the database has already been downloaded and the data set is created
 data_set <- readRDS(file.path(
@@ -14,6 +40,8 @@ data_set <- readRDS(file.path(
   "wqbench",
   "ecotox_ascii_12_11_2025.rds"
 ))
+
+medium_lookup <- .build_wqbench_medium_lookup(data_set)
 
 conn <- rws_connect(file.path(
   "data-raw",
@@ -53,7 +81,8 @@ wqbench_data <-
     Conc = sp_aggre_conc_mg.L,
     Group = trophic_group
   ) %>%
-  mutate(Medium = "Unknown")
+  left_join(medium_lookup, by = c("Chemical", "Species", "effect")) %>%
+  mutate(Medium = coalesce(Medium, "Unknown"))
 
 # remove this comment and run once we have decided on the data set
 usethis::use_data(wqbench_data, overwrite = TRUE)
